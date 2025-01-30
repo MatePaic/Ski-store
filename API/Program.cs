3e64ed704707089944b9b4daeb1b3f59b10031d4
MatePaic <57683081+MatePaic@users.noreply.github.com>
2025-01-30 16:00:45 +0100

    created server side project, added CRUD controllers

---
+++ API/Program.cs
@@ -0,0 +1,20 @@
+using API;
+using Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllers();
+builder.Services.AddDbContext<StoreContext>(opt =>
+{
+    opt.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
+
+var app = builder.Build();
+
+app.MapControllers();
+
+DbInitializer.InitDb(app);
+
+app.Run();
